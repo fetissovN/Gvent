036f8019ed5a8a95d457ff42b86807920722b368
Nick <fetissov.n@gmail.com>
2017-11-21 16:52:40 +0300

    bug chat view fixed

--- src/main/java/com/nick/gvent/dao/user/UserDao.java
+++ src/main/java/com/nick/gvent/dao/user/UserDao.java
@@ -2,11 +2,13 @@ package com.nick.gvent.dao.user;
 
 import com.nick.gvent.entity.User;
 import lombok.NonNull;
+import lombok.extern.log4j.Log4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserDao extends JpaRepository<User,Long>{
+
 
     User findByUsername(String username);
 
